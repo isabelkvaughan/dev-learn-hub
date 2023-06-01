4542e069d2fb3ac4e07e3056d9bb09f7099f50d3
Isabel <isabelkvaughan@Isabels-MacBook-Pro.local>
2023-06-01 20:31:42 +0200

    withAuth added to Dashboard routes

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,33 +1,30 @@
-const router = require('express').Router();
-const { Post, Comment, User } = require('../models');
+const { Post } = require("../models");
 
-// Dashboard route
-router.get('/', async (req, res) => {
-  if (!req.session.loggedIn) {
-    res.redirect('/login');
-    return;
-  }
+// Get dashboard
+const getDashboard = async (req, res) => {
+  // if (!req.session.loggedIn) {
+  //   res.redirect("/login");
+  //   return;
+  // }
   try {
     const posts = await Post.findAll({
-      where: { 
+      where: {
         user_id: req.session.user_id,
-      }
+      },
     });
-    
-    res.render('dashboard', {
+
+    res.render("dashboard", {
       loggedIn: req.session.loggedIn,
       posts: posts, // Pass the retrieved posts to the template
       user: req.session.username, // Pass the username to the template
-      content: req.session.content // Pass the content to the template
+      content: req.session.content, // Pass the content to the template
     });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'Failed to retrieve posts' });
+    res.status(500).json({ error: "Failed to retrieve posts" });
   }
-});
+};
 
-// Render New Post Form
-
-// Create New Post
-
-module.exports = router;
+module.exports = {
+  getDashboard,
+};
--- controllers/index.js
+++ controllers/index.js
@@ -3,12 +3,13 @@ const apiRoutes = require("./api");
 const homeController = require("./homeController.js");
 const dashboardController = require("./dashboardController.js");
 const postController = require("./postController.js");
-
 const withAuth = require("../utils/auth");
 
 router.use("/api", apiRoutes);
 router.use("/", homeController);
-router.use("/dashboard", dashboardController);
 router.use("/post", postController);
+
+// Dashboard Routes
+router.get("/dashboard", withAuth, dashboardController.getDashboard);
 
 module.exports = router;
